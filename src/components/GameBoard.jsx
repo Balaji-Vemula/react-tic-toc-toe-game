f2512ba6ead978be07187bc640aae2df9cf2b3dc
vemula <vemulabalaji1998@gmail.com>
2024-05-08 00:16:25 +0530

    immutable way of updating array/object

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,34 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      // updating the state array or object is a mutable way of updation which will 
+      // update the old value in memory immediately even before setGameboard schedules for the update 
+      // This leads to strange state behavior and bugs
+      // prevGameBoard[rowIndex][colIndex] = 'X';
+      // return prevGameBoard
+
+      // Immutable approach: Create a new game board const with deep coping array and its inner array
+      //  updating it and returning it to schdule to state update for setGameboard
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>

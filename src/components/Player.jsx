e4c813d52400c117739046cdb710cf472943383f
vemula <vemulabalaji1998@gmail.com>
2024-05-08 00:34:45 +0530

    Lifting state up

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol, ...remainingProps }) {
+export default function Player({ initialName, symbol, isActive, ...remainingProps }) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
   function clickHandler() {
@@ -16,7 +16,7 @@ export default function Player({ initialName, symbol, ...remainingProps }) {
     btnCaption = "Save";
   }
   return (
-    <li>
+    <li className={isActive ? 'active': undefined}>
       <span className="player">
         {PlayerElement}
         <span className="player-symbol">{symbol}</span>

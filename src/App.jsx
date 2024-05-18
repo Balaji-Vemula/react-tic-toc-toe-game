fe0f7fff94b9788fe4560c0eb1b00edb58362612
vemula <vemulabalaji1998@gmail.com>
2024-05-19 00:52:40 +0530

    deriving state gameboard from turns state

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,46 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 
-export default function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
+export default function App(rowIndex, colIndex) {
+  const [gameTurns, setGameTurns] = useState([]);
+  const [activePlayer, setActivePlayer] = useState("X");
 
-  function handleSelectSquare(){
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O': 'X');
+  function handleSelectSquare(rowIndex, colIndex) {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    setGameTurns((prevTurns) => {
+      // Immutable way of updating state with depending on prevState
+      let currentPlayer = "X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+
+      // sourcery skip: inline-immediately-returned-variable
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+      
+      return updatedTurns;
+    });
   }
 
   return (
     <main>
       <div id="game-container">
-        <ol id="players" className="highlight-player"> 
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer==='X'}/>
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer==='O'} />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
       <Log />
     </main>
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,31 +1,39 @@
-import { useState } from "react";
-
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
-  function handleSelectSquare(rowIndex, colIndex) {
-    setGameBoard((prevGameBoard) => {
-      // updating the state array or object is a mutable way of updation which will 
-      // update the old value in memory immediately even before setGameboard schedules for the update 
-      // This leads to strange state behavior and bugs
-      // prevGameBoard[rowIndex][colIndex] = 'X';
-      // return prevGameBoard
+export default function GameBoard({ onSelectSquare, turns }) {
+  // Derived state: A state or variable created/updated based on some state which in this case is turns
+  let gameBoard = initialGameBoard;
+  // if the turns is an empty array/object this loop wont run
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
 
-      // Immutable approach: Create a new game board const with deep coping array and its inner array
-      //  updating it and returning it to schdule to state update for setGameboard
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedBoard;
-    });
-    onSelectSquare();
+    gameBoard[row][col] = player;
   }
+  
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  // function handleSelectSquare(rowIndex, colIndex) {
+  //   setGameBoard((prevGameBoard) => {
+  //     // updating the state array or object is a mutable way of updation which will
+  //     // update the old value in memory immediately even before setGameboard schedules for the update
+  //     // This leads to strange state behavior and bugs
+  //     // prevGameBoard[rowIndex][colIndex] = 'X';
+  //     // return prevGameBoard
+
+  //     // Immutable approach: Create a new game board const with deep coping array and its inner array
+  //     //  updating it and returning it to schdule to state update for setGameboard
+  //     const updatedBoard = [
+  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //     ];
+  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //     return updatedBoard;
+  //   });
+  //   onSelectSquare();
+  // }
   return (
     <ol id="game-board">
       {gameBoard.map((row, rowIndex) => (
@@ -33,7 +41,7 @@ export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
                   {playerSymbol}
                 </button>
               </li>

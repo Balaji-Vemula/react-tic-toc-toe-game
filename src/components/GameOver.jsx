575c1c2de8a181d5453a67edf7a61d1ab77e2311
VEMULA BALAJI <41908459+Balaji-Vemula@users.noreply.github.com>
2024-06-01 14:23:23 +0000

    sandbox commit formatting

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,4 +1,4 @@
-export default function GameOver({winner, onRestart}) {
+export default function GameOver({ winner, onRestart }) {
   return (
     <div id="game-over">
       <h2>Game Over!</h2>
@@ -6,7 +6,7 @@ export default function GameOver({winner, onRestart}) {
       {!winner && <p>It's a draw!</p>}
       <p>
         <button onClick={onRestart}>Rematch!</button>
-      </p> 
+      </p>
     </div>
-  )
+  );
 }
